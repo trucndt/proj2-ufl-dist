d0d17508b07e683e5e51301f0fac720972ff3f3d
Truc D T Nguyen <truc.ndtt@gmail.com>
2018-09-26 14:59:08 -0400

    Gossip and Full network

--- lib/proj2.ex
+++ lib/proj2.ex
@@ -1,18 +1,88 @@
 defmodule Proj2 do
-  @moduledoc """
-  Documentation for Proj2.
-  """
+  def start(mAlgo, mTopo, mNumNode) do
+    actorList = for i <- 1..mNumNode  do
+      {:ok, pid} = GossipNode.start_link(self(), [], 0)
+      pid
+    end
 
-  @doc """
-  Hello world.
+    fullNetwork(actorList)
 
-  ## Examples
+    GossipNode.initiate(Enum.at(actorList, 0), "John 3:16")
 
-      iex> Proj2.hello()
-      :world
+    waitForWorkers(mNumNode)
 
-  """
-  def hello do
-    :world
+    IO.puts("DONE")
+  end
+
+  defp waitForWorkers(noActors) do
+    if noActors > 0 do
+      receive do
+        :finish -> waitForWorkers(noActors - 1)
+      end
+    end
+  end
+
+  defp fullNetwork(actorList) do
+    for i <- actorList do
+      IO.inspect(i)
+      list = for j <- actorList  do
+          if j != i do
+            j
+          end
+      end
+      list = Enum.reject(list, &is_nil/1)
+      GossipNode.neighbor(i, list)
+    end
   end
 end
+
+defmodule GossipNode do
+  use GenServer
+
+  def start_link(mParent, mNeighbors, mRcvTimes) do
+    GenServer.start_link(__MODULE__, {mParent, mNeighbors, mRcvTimes}, []) # {:ok, pid}
+  end
+
+  def neighbor(server_pid, mNeighborList) do
+    GenServer.call(server_pid, {:neighbor, mNeighborList})
+  end
+
+  def initiate(server_pid, msg) do
+    GenServer.call(server_pid, {:initiate, msg})
+  end
+
+  def init({mParent, mNeighbors, mRcvTimes}) do
+    {:ok, {mParent, mNeighbors, mRcvTimes}}
+  end
+
+  def handle_call({:neighbor, mNeighborList}, _from, {mParent, mNeighbors, mRcvTimes}) do
+      {:reply, :ok, {mParent, mNeighborList, mRcvTimes}}
+  end
+
+  def handle_call({:initiate, msg}, _from, {mParent, mNeighbors, mRcvTimes}) do
+    randNeighbor = Enum.random(mNeighbors)
+    IO.puts("Choosen neighbor: #{inspect(randNeighbor)}")
+    send randNeighbor, msg
+    {:reply, :ok, {mParent, mNeighbors, mRcvTimes}}
+  end
+
+  def handle_info(msg, {mParent, mNeighbors, mRcvTimes}) do
+    IO.puts "#{Kernel.inspect(self())} Received #{msg} #{mRcvTimes + 1} times"
+    mRcvTimes = mRcvTimes + 1
+
+    if mRcvTimes == 1 do
+      send(mParent, :finish)
+    end
+
+    if mRcvTimes == 10 do
+      {:stop, :normal, {mParent, mNeighbors, mRcvTimes}}
+    else
+      randNeighbor = Enum.random(mNeighbors)
+      send randNeighbor, msg
+      IO.puts "#{Kernel.inspect(self())} sends #{msg} to #{Kernel.inspect(randNeighbor)}"
+      {:noreply, {mParent, mNeighbors, mRcvTimes}}
+    end
+  end
+end
+
+
